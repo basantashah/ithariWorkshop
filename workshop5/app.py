11f5a1fff2a232d71cfe4a07d284b08833ec928a
basantashah <bindassbasanta@hotmail.com>
2019-02-15 12:33:14 +0545

    code revisited:

--- workshop5/app.py
+++ workshop5/app.py
@@ -1,31 +1,33 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-# define the led pin you want to connect
-ledRed = 8
 
+# define actuators GPIOs
+ledRed = 13
+ledGrn = 26
 # initialize GPIO status variables
-ledRedSts = 0
 
+ledRedSts = 0
+ledGrnSts = 0
 
 # Define led pins as output
 GPIO.setup(ledRed, GPIO.OUT)
-
+GPIO.setup(ledGrn, GPIO.OUT)
 # turn leds OFF
 GPIO.output(ledRed, GPIO.LOW)
+GPIO.output(ledGrn, GPIO.LOW)
 
 
 @app.route("/")
 def index():
-    # Read Sensors Status
+    # Read GPIO Status
     ledRedSts = GPIO.input(ledRed)
-
+    ledGrnSts = GPIO.input(ledGrn)
     templateData = {
-        'title': 'GPIO output Status!',
         'ledRed': ledRedSts,
-
+        'ledGrn': ledGrnSts,
     }
     return render_template('index.html', **templateData)
 
@@ -34,6 +36,8 @@ def index():
 def action(deviceName, action):
     if deviceName == 'ledRed':
         actuator = ledRed
+    if deviceName == 'ledGrn':
+        actuator = ledGrn
 
     if action == "on":
         GPIO.output(actuator, GPIO.HIGH)
@@ -41,10 +45,11 @@ def action(deviceName, action):
         GPIO.output(actuator, GPIO.LOW)
 
     ledRedSts = GPIO.input(ledRed)
+    ledGrnSts = GPIO.input(ledGrn)
 
     templateData = {
         'ledRed': ledRedSts,
-
+        'ledGrn': ledGrnSts,
     }
     return render_template('index.html', **templateData)
 
